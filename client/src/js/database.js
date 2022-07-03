964e136878343d2a37d7ee326344d42b74f07bf4
Marcus mbp <maherreraiv1987@gmail.com>
2022-07-03 16:01:02 -0600

    debugging codemirror error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -21,7 +21,7 @@ export const putDb = async (content) => {
 	const jateDb = await openDB('jate', 1);
 	const tx = jateDb.transaction('jate', 'readwrite');
 	const store = tx.objectStore('jate');
-	const request = store.put({ content });
+	const request = store.put({ id: 1, value: content });
 	const result = await request;
 	console.log('data saved to the database', result);
 };
@@ -32,9 +32,9 @@ export const getDb = async () => {
 	const jateDb = await openDB('jate', 1);
 	const tx = jateDb.transaction('jate', 'readonly');
 	const store = tx.objectStore('jate');
-	const request = store.getAll();
+	const request = store.get(1);
 	const result = await request;
-	console.log('result.value', result);
+	console.log('result', result);
 	return result;
 };
 
